02071f6eded41f63840fdfe7733d7d454c78ce92
sousaleal.fabio <sousaleal.fabio@450c7a4d-3b44-9d6e-4ce1-267eaa6a1f9a>
2010-09-21 19:30:13 +0000

    Usuario administrador (medico)

--- neo/grails-app/conf/BootStrap.groovy
+++ neo/grails-app/conf/BootStrap.groovy
@@ -10,22 +10,41 @@ class BootStrap {
 		def roleUser  = new Hierarquia(authority:'ROLE_FUNCIONARIO', description:'Funcionarios da clinica')
 		
 		def userGod = new Usuario(
-		username:'manutencao',
-		userRealName :'Equipe de Manutencao',
-		enabled : true,
-		email : '',
-		passwd : authenticateService.encodePassword('admin'));
+			username:'manutencao',
+			userRealName :'Equipe de Manutencao',
+			enabled : true,
+			email : '',
+			passwd : authenticateService.encodePassword('admin'));
 		
+		def userMed = new Usuario(
+			username:'harrison',
+			userRealName : 'Harrison',
+			enabled : true,
+			email : 'harrison73oft@gmail.com',
+			passwd : authenticateService.encodePassword('harrison'));
+	
+	
 		roleManutencao.save()
 		roleAdmin.save()
 		roleUser.save()
-		
+				
 		if (!userGod.hasErrors()) {
 			userGod.save()
 			roleManutencao.addToPeople(userGod)
 		} else {
-			println "Usuario default com erros!"
+			println "Usuario de manutencao com erros!"
 		}
+		
+		if (!userMed.hasErrors()) {
+			userMed.save()
+			roleAdmin.addToPeople(userMed)
+		} else {
+			println "Usuario administrador com erros!"
+		}
+		
+		
+		
+		
 	}
 	
 	def destroy = {
