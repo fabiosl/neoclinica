0ad390da8efccecc145a621411edfbc68e5d5c34
sousaleal.fabio <sousaleal.fabio@450c7a4d-3b44-9d6e-4ce1-267eaa6a1f9a>
2010-09-21 20:31:18 +0000

    Testes de login completos (medico, secretaria e manutencao).

--- trunk/NeoClinicaTestes/test/functional/LoginTests.groovy
+++ trunk/NeoClinicaTestes/test/functional/LoginTests.groovy
@@ -1,26 +1,62 @@
 
 class LoginTests extends functionaltestplugin.FunctionalTestCase {
 	private String defaultLocation =  "http://localhost:9000/NEOSoftGrails/login/auth";
-	void testAnonymousAccess() {
-			get(this.defaultLocation);
-			assertContentContains("Digite login e senha")
-									
-    }
 	
-	void testManutencaoAccess(){
-
+	private void login(String user, String pass){
+		
+	}
+	
+	void testAnonymousAccess() {
 		get(this.defaultLocation);
 		assertContentContains("Digite login e senha")
-
+	}
+	
+	void testManutencaoAccess(){
+		
+		get(this.defaultLocation);
 		form('loginForm') {
 			j_username = "manutencao"
 			j_password = "admin"
 			click "OK"
-		  }
+		}
 		
 		assertStatus 200
 		assertContentContains "Equipe de Manutencao, bem vindo ao SI Neo!"
 	}
 	
+	void testMedicoAccess(){
+		get(this.defaultLocation);
+		form('loginForm') {
+			j_username = "harrison"
+			j_password = "harrison"
+			click "OK"
+		}
+		
+		assertStatus 200
+		assertContentContains "Harrison, bem vindo ao SI Neo!"
+	}
+	
+	void testSecretariaAccess(){
+		get(this.defaultLocation);
+		form('loginForm') {
+			j_username = "camila"
+			j_password = "camila"
+			click "OK"
+		}
+		
+		assertStatus 200
+		assertContentContains "Camila, bem vindo ao SI Neo!"
+	}
+	
+	void testUnsuccessfullAccess(){
+		get(this.defaultLocation);
+		form('loginForm') {
+			j_username = "camila"
+			j_password = "outropasswd"
+			click "OK"
+		}
+		assertContentContains "Nome ou senha invalidos."
+		
+	}
 	
 }
