568396cdcce6804c88278219557041a8ac58e01f
sousaleal.fabio <sousaleal.fabio@450c7a4d-3b44-9d6e-4ce1-267eaa6a1f9a>
2010-10-20 12:20:31 +0000

    unit tests

---
+++ neo/test/unit/core/LenteTests.groovy
@@ -0,0 +1,27 @@
+package core
+
+import grails.test.GrailsUnitTestCase;
+
+/**
+ * Classe de teste da classe de dominio que modela as lentes do sistema.
+ */
+
+
+class LenteTests extends GrailsUnitTestCase {
+
+	def testCreate() {
+		def l1 = new Lente(
+			tipo : "ZYX",
+			quantidade : 10,
+			valor : 10.2
+		);
+	
+	assertEquals "ZYX", l1.getTipo();
+	assertEquals 10, l1.getQuantidade();
+	float f = 10.2
+	assertEquals f, l1.getValor();
+	
+	}
+	
+}
+
