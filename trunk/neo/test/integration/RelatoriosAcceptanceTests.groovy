6f5a55fe939ee27be569c1c03871f46f7e489b02
sousaleal.fabio <sousaleal.fabio@450c7a4d-3b44-9d6e-4ce1-267eaa6a1f9a>
2010-11-03 18:23:46 +0000

    teste dos relatorios

---
+++ trunk/neo/test/integration/RelatoriosAcceptanceTests.groovy
@@ -0,0 +1,67 @@
+import groovy.util.GroovyTestCase;
+import core.Lente;
+import core.Fornecedor;
+import core.Fornecimento;
+import core.TransacaoEstoque
+
+/**
+ * Testes de aceitacao da user story 11 (Geracao de relatorio)
+ */
+class RelatoriosAcceptanceTests extends GroovyTestCase{
+	void tearDown() {
+//		Lente.list()*.delete();
+//		Fornecedor.list()*.delete();
+//		Fornecimento.list()*.delete();
+	}
+	
+	void test11_1(){
+		Lente.list()*.delete();
+		Fornecedor.list()*.delete();
+		Fornecimento.list()*.delete();
+			assertEquals (Lente.list().size(),0)
+			def lenteValida = new Lente(
+					tipo : 'Look',
+					quantidade : 15,
+					valor : 0f);
+			assertTrue lenteValida.validate()
+			lenteValida.save()
+			
+			assertTrue Lente.list().size() != 0
+			
+	}
+	void test11_2(){
+		
+		Lente.list()*.delete();
+		Fornecedor.list()*.delete();
+		Fornecimento.list()*.delete();
+		
+		def lenteValida = new Lente(
+				tipo : 'Look',
+				quantidade : 15,
+				valor : 0f);
+		assertTrue lenteValida.validate()
+		lenteValida.save()
+
+		def fornecedorValido = new Fornecedor(
+			cnpj : '1234567890',
+			nome : 'OticaACT',
+			endereco : 'Rua Maciel Pinheiro 255, Centro',
+			telefone : '34411312');
+		assertTrue fornecedorValido.validate()
+		fornecedorValido.save()
+		assertNotNull Fornecedor.findByNome('OticaACT')
+		def fornecimentoValido = new Fornecimento(
+			dataPedido : new Date(),
+			dataRecebimento : new Date(),
+			quantidade : 10,
+			preco : 100,
+			lente : lenteValida,
+			fornecedor : fornecedorValido);
+		assertTrue fornecimentoValido.validate()
+		fornecimentoValido.save()
+		def transacoes = TransacaoEstoque.findAllByDataRealizacaoBetween(new Date(), new Date());
+		assertNotNull transacoes
+		
+		}
+		
+}
